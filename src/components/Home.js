854dd0ffecc741353ef7b8ab1360cd1c96d818e1
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-10-25 09:55:00 +0200

    update just before storage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React  from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
-import background from '../img/background.jpg';
+
+import { 
+  DancingScript_400Regular,
+  DancingScript_500Medium,
+  DancingScript_600SemiBold,
+  DancingScript_700Bold 
+} from '@expo-google-fonts/dancing-script'
+
+import { 
+  Gugi_400Regular 
+} from '@expo-google-fonts/gugi'
+
+import { 
+  PermanentMarker_400Regular 
+} from '@expo-google-fonts/permanent-marker'
+
+import { useFonts } from 'expo-font';
+
+import AppLoading from 'expo-app-loading';
+
 
 
 
 function Home() {
+  let [fontsLoaded, error ]=useFonts({
+    DancingScript_400Regular,
+    DancingScript_500Medium,
+    DancingScript_600SemiBold,
+    DancingScript_700Bold,
+    Gugi_400Regular,
+    PermanentMarker_400Regular 
+  
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     //<ImageBackground source={background} resizeMode="cover" style={styles.image}>
       <View style={styles.container}>
@@ -44,24 +77,27 @@ const styles = StyleSheet.create({
     height:300
   },
   text_logo:{
-    fontSize:40,
-    fontWeight:"bold"
+    fontSize:60,
+    fontFamily:"Gugi_400Regular",
   },
   slogan_container:{
     width:"100%",
     marginBottom: 50
   },
   slogan_top:{
-    fontSize:20,
+    fontSize:30,
     width:"100%",
     textAlign:"left",
-    paddingLeft: 20
+    paddingLeft: 20,
+    fontFamily:"DancingScript_400Regular"
   },
   slogan_bot:{
-    fontSize:20,
+    fontSize:30,
     width:"100%",
     textAlign:"right",
     paddingRight: 20,
+    fontFamily:"DancingScript_400Regular"
+
   }
     
 });
--- src/components/TestRef.js
+++ src/components/TestRef.js
@@ -2,141 +2,27 @@ import React, { useState , useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView,  ImageBackground,TouchableOpacity, TouchableHighlight, RefreshControl} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
-import Product from './Product';
-import background2 from '../img/background2.png';
+
 
 
 
 function FindPage() {
 
-    const [barCode, setBarcode] = useState();
-    const [data, setData] = useState();
-    const [message, setMessage] = useState();
-    const [refresh, setRefresh] = useState();
+    
 
-    function findData(barCode) {
-        console.log(barCode);
-        getInfo(barCode);
-        setBarcode("")
-        
-    }
-
-    const getInfo = async (barCode) => {
-      fetch(`https://world.openfoodfacts.org/api/v0/product/${barCode}.json`)
-         .then((res) => {
-           res.json()
-           .then((reponse) => {
-             console.log(reponse.status);
-             if (reponse.status === 1) {
-               setMessage()
-               setData(reponse)
-             } else {
-               setData()
-               setMessage("Aucune données trouvées pour ce Code Barre")
-               setTimeout(function(){ setMessage()}, 3000);
-             }
-           })
-         })
-         .catch((err) => console.log(err))
-     }
-
-     function clearData() {
-       setRefresh(true);
-       setData()
-       setRefresh(false);
-     }
-     
+   
 
   return (
-    <ImageBackground source={background2} resizeMode="cover" style={styles.image}>
-        <ScrollView style={styles.body}
-            refreshControl={
-            <RefreshControl
-                refreshing={refresh}
-                onRefresh={clearData}/>}>
-            <View style={styles.find_top}>
-                <Text style={styles.title}>Entrez un code barre :</Text>
-                <TextInput 
-                    style={styles.input}
-                    value={barCode}
-                    onChangeText={setBarcode}
-                    placeholder="ex : 3245413125316"
-                    keyboardType="numeric"
-                />
-                <TouchableHighlight style={styles.button_find}
-                onPress={()=> findData(barCode)}
-                activeOpacity={0.5}
-                underlayColor={"#00867d"}
-                >
-                    <Text>Rechercher</Text>
-                </TouchableHighlight>
-            </View>
-            {data && 
-                <View style={styles.scroll}>
-                    <Product data={data}/>
-                </View>
-            }
-            {message && 
-                <View style={styles.scroll}>
-                    <Text style={styles.message}>{message}</Text>
-                </View>
-            }
+        <ScrollView style={styles.body}>
+            <Text>Page de test</Text>
         </ScrollView>
-    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   body:{
       flex:1,
-  },
-  image:{
-    flex:1,
-  },
-  find_top:{
-    width: "100%",
-    alignItems:"center",
-  },
-  title:{
-    fontSize: 20,
-    marginTop:20,
-    marginBottom:20,
-  },
-  input:{
-      borderWidth: 2,
-      height:40,
-      width:"75%",
-      backgroundColor:"white",
-      textAlign:"center",
-      marginBottom:20,
-  },
-  button_find:{
-      backgroundColor: "white",
-      width:"50%",
-      height:40,
-      justifyContent:"center",
-      alignItems:"center",
-      borderRadius: 20,
-      borderColor: "#00867d",
-      borderWidth:2,
-      marginBottom:20,
-  },
-  scroll:{
-    flex:1,
-    width:"100%",
-    alignItems:"center"
-  },
-  message:{
-    marginTop:20,
-    fontSize:20,
-    width:"80%",
-    textAlign:"center",
-    backgroundColor:"white",
-    padding:10,
-    borderRadius:10,
-    borderColor:"#00867d",
-    borderWidth:2,
-  },
+  } 
 });
 
 export default FindPage;
